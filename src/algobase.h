0e10089a53154d332b3f2ed3065a56b8c90fe190
LiCan <1796183504@qq.com>
2022-02-20 13:43:04 +0800

    2022/02/03

--- src/algobase.h
+++ src/algobase.h
@@ -2,9 +2,43 @@
 #define MYSTL_ALGOBASE_H
 namespace mystl{
     template<typename T>
+    const T &max(const T&lhs,const T& rhs)
+    {
+        //test if there is operator <
+        return lhs<rhs?rhs:lhs;
+    }
+    template<typename T,typename Compare>
+    const T& max(const T&lhs,const T& rhs,Compare comp)
+    {
+        return comp(lhs,rhs)?rhs:lhs;
+    }
     
+    template<typename T>
+    const T &min(const T&lhs,const T& rhs)
+    {
+        //test if type T has operator <
+        return lhs<rhs?lhs:rhs;
+    }
+    template<typename T,typename Compare>
+    const T& min(const T&lhs,const T& rhs,Compare comp)
+    {
+        //test if Compare has function ()
+        return comp(lhs,rhs)?lhs:rhs;
+    }
 
+    template<typename T>
+    inline void swap(T& a,T &b)
+    {
+        T tmp=a;
+        a=b;
+        b=tmp;
+    }
 
+    template<typename T1,typename T2>
+    void iter_swap(T1 lhs,T2 rhs)
+    {
+        mystl::swap(*lhs,*rhs);
+    }
 
 
 }
