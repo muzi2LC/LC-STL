75c49714fcc1172fb73308a1582f032a1033b4e4
Li Can <1796183504@qq.com>
2022-02-24 08:38:33 +0800

    2022/02/24

---
+++ src/iterator.h
@@ -0,0 +1,19 @@
+#ifndef MYSTL_ITERATOR_H
+#define MYSTL_ITERATOR_H
+#include"traits.h"
+namespace mystl
+{
+    struct input_iterator_tag{};
+    struct output_iterator_tag{};
+    struct forward_iterator_tag:public input_iterator_tag{};
+    struct bidirectional_iterator_tag:public forward_iterator_tag{};
+    struct random_access_iterator_tag:public bidirectional_iterator_tag{;
+
+
+    
+}
+
+
+
+
+#endif
